61b87f0d9a8b3ea89e7804a309910b5b0c83b9fa
mahfuz380 <almahfuz380@gmail.com>
2021-04-05 13:22:43 +0600

    done

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+$conn = new mysqli('localhost','root','','textdata');
+
+$file = fopen("DATA.txt", 'r');
+
+while(!feof($file)){
+    $content = fgets($file);
+    
+    $sql = "INSERT INTO `data` ( `data`) VALUES ('$content')";
+
+    $conn->query($sql);
+}
+
+
+fclose($file);
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="/store" method="post" enctype="multipart/form-data">
+    
+        <input type="file" name="file">
+        <button class="btn btn-primary" type="submit">Click Here</button>
+    </form>
+</body>
+
+</html>
